72ab959cd665c883f528b189b84d4eda7eb9b41f
Felipe Alarcon <felipe.alarcon1401@alumnos.ubiobio.cl>
2020-07-31 22:27:31 -0400

    Creación Model Producto.

---
+++ src/app/Models/producto.models.ts
@@ -0,0 +1,15 @@
+export class ProductoModel{
+  id:number;
+  nombre:string;
+  precio:number;
+  stock:number;
+  imagen:string;
+  Categoria_id:number;
+  Marca_id:number;
+
+
+  constructor(){
+
+  }
+
+}

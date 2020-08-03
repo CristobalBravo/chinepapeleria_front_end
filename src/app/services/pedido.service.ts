48ae44a2ec7e2a630a3bc4dee724dbf6e3dde722
Felipe Alarcon <felipe.alarcon1401@alumnos.ubiobio.cl>
2020-08-02 20:28:02 -0400

    Implementacion de obtener : - Todos los pedidos en servicio Pedido. - Todos los detalles Pedidos en servicio detalle Pedido. - Correción de ruta utilizando global.

--- src/app/services/pedido.service.ts
+++ src/app/services/pedido.service.ts
@@ -1,17 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import {global} from './global';
+import {map} from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PedidoService {
+  url:string;
 
-  private url='http://localhost:8000/api/pedido/';
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) {
+    this.url=global.url;
+  }
 
   crearPedido(pedido,token): Observable<any>{
     let headers= new HttpHeaders().set('Authorization',token);
-    return this.http.post(this.url+'crear',pedido,{headers:headers});
+    return this.http.post(this.url+'pedido/crear',pedido,{headers:headers});
+    }
+
+    all(){
+      let token = localStorage.getItem('token');
+      let headers = new HttpHeaders().set('Authorization',token);
+      return this.http.post(this.url+'pedido/all', null,{headers:headers} ).pipe(map(this.crearArreglo));
+    }
+    private crearArreglo(pedido:object){
+      const pedidoArray: any[]=[];
+      if(pedido === null){return [];}
+
+      Object.keys(pedido).forEach(id=>{
+        const movies:any =pedido[id];
+        pedidoArray.push(movies);
+      })
+      return pedidoArray;
     }
 }
